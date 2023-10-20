229925e6fd7eb3cf58493403a495db1fd743bbfc
Leonides2 <114005429+Leonides2@users.noreply.github.com>
2023-10-20 09:58:21 -0600

    base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, TypeOrmModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+@Module({
+  imports: [BooksModule, TypeOrmModule.forRoot({
+    type: 'mariadb',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'soporte',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
