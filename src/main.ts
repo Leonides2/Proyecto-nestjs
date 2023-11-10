b350ce6a4de8324296630689d035c3f4029681c0
Leonides2 <114005429+Leonides2@users.noreply.github.com>
2023-11-10 09:16:28 -0600

    Implementacion de cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://http://127.0.0.1:5500'
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
